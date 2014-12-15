5388d0a9bf9e6dd0be8b1982c63d9cf681fbce2b
joyghosh <joy.ghosh@tiatros.com>
2014-12-15 11:45:56 +0000

    [Joy Ghosh] Threadpool first commit

---
+++ src/com/joy/threadpool/ThreadPool.java
@@ -0,0 +1,45 @@
+package com.joy.threadpool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+public class ThreadPool {
+	
+	private BlockingQueue<Runnable> taskQueue = null;
+	private List<WorkerThread> threads = new ArrayList<WorkerThread>();
+	private boolean isInActive = false;
+	private boolean isTaskQueueDynamic = false;
+	
+	/**
+	 * Dynamic number of tasks can be added to
+	 * Task Queue.
+	 * @param noOfWorkerThreads
+	 */
+	public ThreadPool(int noOfWorkerThreads){
+		this.isTaskQueueDynamic = true;
+		
+		//creating workers in the pool.
+		for(int i = 0; i< noOfWorkerThreads;i++){
+			threads.add(new WorkerThread());
+		}
+		
+		//starting the worker threads in pool.
+		for(WorkerThread w:threads){
+			w.start();
+		}
+	}
+	
+	/**
+	 * Fixed sized task queue.
+	 * @param noOfWorkerThreads
+	 * @param noOfTasks
+	 */
+	public ThreadPool(int noOfWorkerThreads,int noOfTasks){
+		taskQueue = BlockingQueue<Runnable>(noOfTasks);
+	}
+	
+	public void addTask(Runnable task){
+//		this.taskQueue.
+	}
+}
---
+++ src/com/joy/threadpool/WorkerThread.java
@@ -0,0 +1,18 @@
+package com.joy.threadpool;
+
+import java.util.concurrent.BlockingQueue;
+
+public class WorkerThread extends Thread{
+	private BlockingQueue taskQueue = null;
+	private boolean isStopped = false;
+	
+	public WorkerThread(){
+		
+	}
+	
+	private synchronized Runnable dequeue(){
+		
+		
+		return null;	
+	}
+}
