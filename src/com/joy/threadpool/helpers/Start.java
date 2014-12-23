dc1bb6c1de6e2fe703ef00271c27441834c84d5a
joyghosh <joy.ghosh@tiatros.com>
2014-12-23 12:47:41 +0000

    Batch jobs error

--- src/com/joy/threadpool/helpers/Start.java
+++ src/com/joy/threadpool/helpers/Start.java
@@ -1,44 +1,58 @@
 package com.joy.threadpool.helpers;
 
 import com.joy.threadpool.ThreadPool;
+import com.joy.threadpool.samples.BatchTask;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ThreadPool tp = new ThreadPool(5,5000);
-		
 		System.out.println("Starting main");
 		
-		for(int i=0;i<5050;i++){
-			try {
-				tp.execute(new PrintThread(("Thread "+Integer.toString(i)),Integer.toString(i)));
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println(e.getMessage());
-				break;
-			}
-		}
+//		ThreadPool tp = new ThreadPool(5,5000);
+//		
+//		for(int i=0;i<5050;i++){
+//			try {
+//				tp.execute(new PrintThread(("Thread "+Integer.toString(i)),Integer.toString(i)));
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				System.out.println(e.getMessage());
+//				break;
+//			}
+//		}
+//		
+//		try {
+//			
+//			Thread.sleep(30000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	
+//		System.out.println("2nd Batch of jobs.");
+//		for(int i=5002;i<10000;i++){
+//			try {
+//				tp.execute(new PrintThread(("Thread "+Integer.toString(i)),Integer.toString(i)));
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				System.out.println(e.getMessage());
+//				break;
+//			}
+//		}
+//		
+//		try {
+//			
+//			Thread.sleep(20000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		
+//		tp.deactivate();
 		
-		try {
-			
-			Thread.sleep(30000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		System.out.println("2nd Batch of jobs.");
-		for(int i=5002;i<10000;i++){
-			try {
-				tp.execute(new PrintThread(("Thread "+Integer.toString(i)),Integer.toString(i)));
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println(e.getMessage());
-				break;
-			}
-		}
+		new BatchTask(5);
 		
 		try {
 			
@@ -47,9 +61,6 @@ public class Start {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		tp.deactivate();
 		
         System.out.println("Ending main");
 	}
---
+++ src/com/joy/threadpool/samples/BatchTask.java
@@ -0,0 +1,51 @@
+package com.joy.threadpool.samples;
+
+import java.util.Collection;
+import java.util.Random;
+
+import com.joy.threadpool.ThreadPool;
+import com.joy.threadpool.helpers.PrintThread;
+
+public class BatchTask{
+		
+		private Collection<Runnable> tasks;
+		private ThreadPool tp;
+		private int THREAD_POOL_SIZE;
+		private int noOfTasks = 0;		
+		
+		public BatchTask(int THREAD_POOL_SIZE){
+				this.THREAD_POOL_SIZE = THREAD_POOL_SIZE;
+				init();
+		} 
+		
+		public BatchTask(int THREAD_POOL_SIZE,int noOfTasks){
+				this.THREAD_POOL_SIZE = THREAD_POOL_SIZE;
+				this.noOfTasks = noOfTasks;										//overwrite the number of tasks.
+				init();
+		}
+		
+		public void init(){
+			
+			if(this.noOfTasks == 0){
+				this.tp = new ThreadPool(this.THREAD_POOL_SIZE);
+				this.noOfTasks = new Random().nextInt() + 1;					//minimum 1 task in the batch.
+			}
+			else{
+				this.tp = new ThreadPool(this.THREAD_POOL_SIZE,this.noOfTasks);
+			}
+			
+			for(int i = 0;i<this.noOfTasks;i++){
+				this.tasks.add(new PrintThread("Thread "+Integer.toString(i),Integer.toString(i)));
+			}
+		}
+		
+		public void startBatchTask(){
+			
+			try {
+				this.tp.execute(this.tasks);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Exception: "+e.getMessage());
+			}
+		}
+}
